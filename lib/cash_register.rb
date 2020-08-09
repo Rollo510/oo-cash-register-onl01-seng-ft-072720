a30ecba2736ccc109b8eb4d003f9fc657c8ab78e
Kevin Britt <kbritt510@gmail.com>
2020-08-09 08:10:38 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ class CashRegister
   end
   
   def apply_discount
-    @total -= @total * @discount.to_f
+    @total -= @total * @discount.to_f / 100
       if @discount == 0
         "There is no discount to apply."
       else
