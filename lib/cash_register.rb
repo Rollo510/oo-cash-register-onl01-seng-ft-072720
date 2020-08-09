e110b03ea40c0697f337078cdd8ffecd8d33ea9d
Kevin Britt <kbritt510@gmail.com>
2020-08-09 08:05:34 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,7 +7,7 @@ class CashRegister
   def initialize(discount = 20)
     @total = 0
     @discount = discount
-    self.class.all << self
+    @@items << self
   end
   
   def add_item(title, price, quantity = 1)
