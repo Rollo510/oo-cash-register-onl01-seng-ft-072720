66ab9be26eb9bbd88b21dd857279e245f37d1b1f
Kevin Britt <kbritt510@gmail.com>
2020-08-09 05:07:24 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,17 +2,11 @@ class CashRegister
 
 attr_accessible = :total, :discount 
 
-def initialize(total = 0, discount = nil)
+def initialize(discount = 0)
   @total = total
   @discount = discount
-end
-
-def self.discount
-  @@discount
-end
-
-def self.total
-  @@total
+  total = 0
+  
 end
 
 
