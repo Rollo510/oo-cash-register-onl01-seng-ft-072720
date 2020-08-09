049c81239231910898bf598c45c5f909c8ee9bcb
Kevin Britt <kbritt510@gmail.com>
2020-08-09 05:12:41 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,14 +1,9 @@
 class CashRegister
 
-attr_accessor = :total, :discount 
+  attr_accessor :total, :discount, :last_transaction_amount, :items
 
-def initialize(discount = 0)
-  @total = 0
-  @discount = discount
-  
-end
-
-
-
-
-end
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
